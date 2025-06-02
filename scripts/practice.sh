ff580e293d983e0b8aeeb9f790775b3cf3aba3e3
maton369 <ysk901085@gmail.com>
2025-06-02 12:48:44 +0900

    Add: 通信ができるようなったことを確認

--- scripts/practice.sh
+++ scripts/practice.sh
@@ -15,5 +15,11 @@
 # ip netns exec ns1 ip address add 192.0.2.1/24 dev ns1-veth0
 # ip netns exec ns2 ip address add 192.0.2.2/24 dev ns2-veth0
 
-ip netns exec ns1 ip link show ns1-veth0 | grep state | tee output.log
-ip netns exec ns2 ip link show ns2-veth0 | grep state | tee -a output.log
+# ip netns exec ns1 ip link show ns1-veth0 | grep state | tee output.log
+# ip netns exec ns2 ip link show ns2-veth0 | grep state | tee -a output.log
+
+ip netns exec ns1 ip link set ns1-veth0 up
+ip netns exec ns2 ip link set ns2-veth0 up
+
+ip netns exec ns1 ping -c 3 192.0.2.2 | tee output.log
+ip netns exec ns2 ping -c 3 192.0.2.1 | tee -a output.log
