faaed42e5874c8363b8bb532e4ff2372245923cb
maton369 <ysk901085@gmail.com>
2025-06-02 12:43:38 +0900

    Add: vethインターフェースにIPアドレスを付与

--- scripts/practice.sh
+++ scripts/practice.sh
@@ -9,5 +9,8 @@
 # ip link set ns2-veth0 netns ns2
 # ip link show | grep veth | tee output.log
 
-ip netns exec ns1 ip link show | grep veth | tee -a output.log
-ip netns exec ns2 ip link show | grep veth | tee -a output.log
+# ip netns exec ns1 ip link show | grep veth | tee -a output.log
+# ip netns exec ns2 ip link show | grep veth | tee -a output.log
+
+ip netns exec ns1 ip address add 192.0.2.1/24 dev ns1-veth0
+ip netns exec ns2 ip address add 192.0.2.2/24 dev ns2-veth0
