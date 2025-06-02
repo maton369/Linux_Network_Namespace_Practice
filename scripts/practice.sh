94105523877923d0c1da850d7286de71666573e4
maton369 <ysk901085@gmail.com>
2025-06-02 12:33:03 +0900

    Add: helloworld NetworkNamespaceを削除

--- scripts/practice.sh
+++ scripts/practice.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
 #ip netns add helloworld | tee output.log
-ip netns list | tee output.log
+
+#ip netns exec helloworld ip route show | tee output.log
+
+ip netns delet helloworld | tee output.log
