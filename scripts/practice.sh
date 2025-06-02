1e3b5816f1f69f4d9710d6c3ce74a6b254ee8dee
maton369 <ysk901085@gmail.com>
2025-06-02 13:36:07 +0900

    Add: 通信ができるか確認

--- scripts/practice.sh
+++ scripts/practice.sh
@@ -52,11 +52,14 @@
 # ip netns exec router ip link show gw-veth1 | grep state | tee -a output.log
 # ip netns exec ns2 ip link show ns2-veth0 | grep state | tee -a output.log
 
-ip netns exec ns1 ip address add 192.0.2.1/24 dev ns1-veth0
-ip netns exec router ip address add 192.0.2.254/24 dev gw-veth0
-ip netns exec router ip address add 198.51.100.254/24 dev gw-veth1
-ip netns exec ns2 ip address add 198.51.100.1/24 dev ns2-veth0
-ip netns exec ns1 ip addr show ns1-veth0 | tee output.log
-ip netns exec router ip addr show gw-veth0 | tee -a output.log
-ip netns exec router ip addr show gw-veth1 | tee -a output.log
-ip netns exec ns2 ip addr show ns2-veth0 | tee -a output.log
+# ip netns exec ns1 ip address add 192.0.2.1/24 dev ns1-veth0
+# ip netns exec router ip address add 192.0.2.254/24 dev gw-veth0
+# ip netns exec router ip address add 198.51.100.254/24 dev gw-veth1
+# ip netns exec ns2 ip address add 198.51.100.1/24 dev ns2-veth0
+# ip netns exec ns1 ip addr show ns1-veth0 | tee output.log
+# ip netns exec router ip addr show gw-veth0 | tee -a output.log
+# ip netns exec router ip addr show gw-veth1 | tee -a output.log
+# ip netns exec ns2 ip addr show ns2-veth0 | tee -a output.log
+
+ip netns exec ns1 ping -c 3 192.0.2.254 -I 192.0.2.1 | tee output.log
+ip netns exec ns2 ping -c 3 198.51.100.254 -I 198.51.100.1 | tee -a output.log
