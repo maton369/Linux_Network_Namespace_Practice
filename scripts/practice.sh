d1aac785f32637ce218f7d92570808111b371a3f
maton369 <ysk901085@gmail.com>
2025-06-02 12:37:53 +0900

    Add: ネットワークインターフェースを作成

--- scripts/practice.sh
+++ scripts/practice.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-#ip netns add helloworld | tee output.log
-
-#ip netns exec helloworld ip route show | tee output.log
-
-ip netns delet helloworld | tee output.log
+ip netns add ns1
+ip netns add ns2
+ip link add ns1-veth0 type veth peer name ns2-veth0
+ip link show | grep veth | tee output.log
