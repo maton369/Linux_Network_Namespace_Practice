7db160c7c76f71fe567e93405c6aa0c09748d0e7
maton369 <ysk901085@gmail.com>
2025-06-02 12:41:49 +0900

    Add: Network Namespace同士を仮想的なLANケーブルで接続

--- scripts/practice.sh
+++ scripts/practice.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-ip netns add ns1
-ip netns add ns2
-ip link add ns1-veth0 type veth peer name ns2-veth0
-ip link show | grep veth | tee output.log
+# ip netns add ns1
+# ip netns add ns2
+# ip link add ns1-veth0 type veth peer name ns2-veth0
+# ip link show | grep veth | tee output.log
+
+# ip link set ns1-veth0 netns ns1
+# ip link set ns2-veth0 netns ns2
+# ip link show | grep veth | tee output.log
+
+ip netns exec ns1 ip link show | grep veth | tee -a output.log
+ip netns exec ns2 ip link show | grep veth | tee -a output.log
