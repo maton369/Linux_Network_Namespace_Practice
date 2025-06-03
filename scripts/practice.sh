aff476adfef5effc29fa7f9de23e12df62463ca6
maton369 <ysk901085@gmail.com>
2025-06-03 09:52:53 +0900

    Add: インターフェースにIPアドレスを付与

--- scripts/practice.sh
+++ scripts/practice.sh
@@ -98,10 +98,18 @@
 # ip link set ns2-veth0 netns ns2
 # ip netns exec ns1 ip link show | tee output.log
 
-ip netns exec ns1 ip link set ns1-veth0 up
-ip netns exec router1 ip link set gw1-veth0 up
-ip netns exec router1 ip link set gw1-veth1 up
-ip netns exec router2 ip link set gw2-veth0 up
-ip netns exec router2 ip link set gw2-veth1 up
-ip netns exec ns2 ip link set ns2-veth0 up
-ip netns exec ns1 ip link show ns1-veth0 | grep state | tee output.log
+# ip netns exec ns1 ip link set ns1-veth0 up
+# ip netns exec router1 ip link set gw1-veth0 up
+# ip netns exec router1 ip link set gw1-veth1 up
+# ip netns exec router2 ip link set gw2-veth0 up
+# ip netns exec router2 ip link set gw2-veth1 up
+# ip netns exec ns2 ip link set ns2-veth0 up
+# ip netns exec ns1 ip link show ns1-veth0 | grep state | tee output.log
+
+ip netns exec ns1 ip address add 192.0.2.1/24 dev ns1-veth0
+ip netns exec router1 ip address add 192.0.2.254/24 dev gw1-veth0
+ip netns exec router1 ip address add 203.0.113.1/24 dev gw1-veth1
+ip netns exec router2 ip address add 203.0.113.2/24 dev gw2-veth0
+ip netns exec router2 ip address add 198.51.100.254/24 dev gw2-veth1
+ip netns exec ns2 ip address add 198.51.100.1/24 dev ns2-veth0
+ip netns exec ns1 ip addr show ns1-veth0 | tee output.log
