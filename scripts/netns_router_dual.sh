b3c6ff098552fe4a16327ef1434ecdb23e310dda
maton369 <ysk901085@gmail.com>
2025-06-03 10:16:30 +0900

    Clean: ファイル分け

---
+++ scripts/netns_router_dual.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+# ip netns add ns1
+# ip netns add router1
+# ip netns add router2
+# ip netns add ns2
+
+# ip link add ns1-veth0 type veth peer name gw1-veth0
+# ip link add gw1-veth1 type veth peer name gw2-veth0
+# ip link add gw2-veth1 type veth peer name ns2-veth0
+
+# ip link set ns1-veth0 netns ns1
+# ip link set gw1-veth0 netns router1
+# ip link set gw1-veth1 netns router1
+# ip link set gw2-veth0 netns router2
+# ip link set gw2-veth1 netns router2
+# ip link set ns2-veth0 netns ns2
+
+# ip netns exec ns1 ip link set lo up
+# ip netns exec router1 ip link set lo up
+# ip netns exec router2 ip link set lo up
+# ip netns exec ns2 ip link set lo up
+
+# ip netns exec ns1 ip link set ns1-veth0 up
+# ip netns exec router1 ip link set gw1-veth0 up
+# ip netns exec router1 ip link set gw1-veth1 up
+# ip netns exec router2 ip link set gw2-veth0 up
+# ip netns exec router2 ip link set gw2-veth1 up
+# ip netns exec ns2 ip link set ns2-veth0 up
+
+# ip netns exec ns1 ip addr add 192.0.2.1/24 dev ns1-veth0
+# ip netns exec router1 ip addr add 192.0.2.254/24 dev gw1-veth0
+# ip netns exec router1 ip addr add 203.0.113.1/24 dev gw1-veth1
+# ip netns exec router2 ip addr add 203.0.113.2/24 dev gw2-veth0
+# ip netns exec router2 ip addr add 198.51.100.254/24 dev gw2-veth1
+# ip netns exec ns2 ip addr add 198.51.100.1/24 dev ns2-veth0
+
+# ip netns exec ns1 ip route add default via 192.0.2.254
+# ip netns exec ns2 ip route add default via 198.51.100.254
+
+# ip netns exec router1 ip route add 198.51.100.0/24 via 203.0.113.2 dev gw1-veth1
+# ip netns exec router2 ip route add 192.0.2.0/24 via 203.0.113.1 dev gw2-veth0
+
+# ip netns exec ns1 ping -c 3 198.51.100.1 -I 192.0.2.1 | tee output.log
+
---
+++ scripts/netns_simple_2node.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# ip netns add ns1
+# ip netns add ns2
+# ip link add ns1-veth0 type veth peer name ns2-veth0
+# ip link show | grep veth | tee output.log
+
+# ip link set ns1-veth0 netns ns1
+# ip link set ns2-veth0 netns ns2
+# ip link show | grep veth | tee output.log
+
+# ip netns exec ns1 ip link show | grep veth | tee -a output.log
+# ip netns exec ns2 ip link show | grep veth | tee -a output.log
+
+# ip netns exec ns1 ip address add 192.0.2.1/24 dev ns1-veth0
+# ip netns exec ns2 ip address add 192.0.2.2/24 dev ns2-veth0
+
+# ip netns exec ns1 ip link show ns1-veth0 | grep state | tee output.log
+# ip netns exec ns2 ip link show ns2-veth0 | grep state | tee -a output.log
+
+# ip netns exec ns1 ip link set ns1-veth0 up
+# ip netns exec ns2 ip link set ns2-veth0 up
+
+# ip netns exec ns1 ping -c 3 192.0.2.2 | tee output.log
+# ip netns exec ns2 ping -c 3 192.0.2.1 | tee -a output.log
+
+# ip --all netns delete
