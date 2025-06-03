ad5d3b837532327ab2cc2a7164bd89efbf50472c
maton369 <ysk901085@gmail.com>
2025-06-03 10:11:02 +0900

    Add: 異なるセグメント間を異なるRouterで通信できることを確認

--- scripts/practice.sh
+++ scripts/practice.sh
@@ -79,16 +79,15 @@
 # ip --all netns delete
 # ip netns list | tee output.log
 
+
 # ip netns add ns1
 # ip netns add router1
 # ip netns add router2
 # ip netns add ns2
-# ip netns list | tee output.log
 
 # ip link add ns1-veth0 type veth peer name gw1-veth0
 # ip link add gw1-veth1 type veth peer name gw2-veth0
 # ip link add gw2-veth1 type veth peer name ns2-veth0
-# ip link show | grep veth | tee output.log
 
 # ip link set ns1-veth0 netns ns1
 # ip link set gw1-veth0 netns router1
@@ -96,7 +95,11 @@
 # ip link set gw2-veth0 netns router2
 # ip link set gw2-veth1 netns router2
 # ip link set ns2-veth0 netns ns2
-# ip netns exec ns1 ip link show | tee output.log
+
+# ip netns exec ns1 ip link set lo up
+# ip netns exec router1 ip link set lo up
+# ip netns exec router2 ip link set lo up
+# ip netns exec ns2 ip link set lo up
 
 # ip netns exec ns1 ip link set ns1-veth0 up
 # ip netns exec router1 ip link set gw1-veth0 up
@@ -104,21 +107,18 @@
 # ip netns exec router2 ip link set gw2-veth0 up
 # ip netns exec router2 ip link set gw2-veth1 up
 # ip netns exec ns2 ip link set ns2-veth0 up
-# ip netns exec ns1 ip link show ns1-veth0 | grep state | tee output.log
 
-# ip netns exec ns1 ip address add 192.0.2.1/24 dev ns1-veth0
-# ip netns exec router1 ip address add 192.0.2.254/24 dev gw1-veth0
-# ip netns exec router1 ip address add 203.0.113.1/24 dev gw1-veth1
-# ip netns exec router2 ip address add 203.0.113.2/24 dev gw2-veth0
-# ip netns exec router2 ip address add 198.51.100.254/24 dev gw2-veth1
-# ip netns exec ns2 ip address add 198.51.100.1/24 dev ns2-veth0
-ip netns exec ns2 ip route add default via 198.51.100.254
-# ip netns exec ns1 ip addr show ns1-veth0 | tee output.log
+# ip netns exec ns1 ip addr add 192.0.2.1/24 dev ns1-veth0
+# ip netns exec router1 ip addr add 192.0.2.254/24 dev gw1-veth0
+# ip netns exec router1 ip addr add 203.0.113.1/24 dev gw1-veth1
+# ip netns exec router2 ip addr add 203.0.113.2/24 dev gw2-veth0
+# ip netns exec router2 ip addr add 198.51.100.254/24 dev gw2-veth1
+# ip netns exec ns2 ip addr add 198.51.100.1/24 dev ns2-veth0
 
-# ip netns exec ns1 ping -c 3 198.51.100.1 -I 192.0.2.1 | tee output.log
+# ip netns exec ns1 ip route add default via 192.0.2.254
+# ip netns exec ns2 ip route add default via 198.51.100.254
 
-# ip netns exec router1 ip route add 198.51.100.0/24 via 203.0.113.1
-# ip netns exec router2 ip route add 192.0.2.0/24 via 203.0.113.1
-# ip netns exec router1 ip route show | tee output.log
+# ip netns exec router1 ip route add 198.51.100.0/24 via 203.0.113.2 dev gw1-veth1
+# ip netns exec router2 ip route add 192.0.2.0/24 via 203.0.113.1 dev gw2-veth0
 
-# ip netns exec ns1 ping -c 3 198.51.100.1 -I 192.0.2.1 | tee output.log
+ip netns exec ns1 ping -c 3 198.51.100.1 -I 192.0.2.1 | tee output.log
