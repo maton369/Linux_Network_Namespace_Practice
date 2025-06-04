cdcb1ce5ae48f04ab33b51c8ebc589b75490703d
maton369 <ysk901085@gmail.com>
2025-06-04 13:26:08 +0900

    Add: インターフェースのMACアドレス

--- scripts/practice.sh
+++ scripts/practice.sh
@@ -32,7 +32,16 @@
 # ip netns exec router ip addr show gw-veth1 | tee -a output.log
 # ip netns exec ns2 ip addr show ns2-veth0 | tee -a output.log
 
-ip netns exec ns1 ip route add default via 192.0.2.254
-ip netns exec ns2 ip route add default via 198.51.100.254
-ip netns exec ns1 ip route show | tee output.log
-ip netns exec ns2 ip route show | tee -a output.log
+# ip netns exec ns1 ip route add default via 192.0.2.254
+# ip netns exec ns2 ip route add default via 198.51.100.254
+# ip netns exec ns1 ip route show | tee output.log
+# ip netns exec ns2 ip route show | tee -a output.log
+
+ip netns exec ns1 ip link set ns1-veth0 address 00:00:5E:00:53:11
+ip netns exec router ip link set gw-veth0 address 00:00:5E:00:53:12
+ip netns ex ec router ip link set gw-veth1 address 00:00:5E:00:53:21
+ip netns exec ns2 ip link set ns2-veth0 address 00:00:5E:00:53:22
+ip netns exec ns1 ip link show dev ns1-veth0 | grep ether | tee output.log
+ip netns exec router ip link show dev gw-veth0 | grep ether | tee -a output.log
+ip netns exec router ip link show dev gw-veth1 | grep ether | tee -a output.log
+ip netns exec ns2 ip link show dev ns2-veth0 | grep ether | tee -a output.log
