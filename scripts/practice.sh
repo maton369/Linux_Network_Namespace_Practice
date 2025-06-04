c41968e482848c0edac2c11dfbed4237c9b804a7
maton369 <ysk901085@gmail.com>
2025-06-04 13:14:08 +0900

    Add: デフォルトルート

--- scripts/practice.sh
+++ scripts/practice.sh
@@ -23,11 +23,16 @@
 # ip netns exec ns2 ip link set ns2-veth0 up
 # ip netns exec ns1 ip link show ns1-veth0 | grep state | tee output.log
 
-ip netns exec ns1 ip address add 192.0.2.1/24 dev ns1-veth0
-ip netns exec router ip address add 192.0.2.254/24 dev gw-veth0
-ip netns exec router ip address add 198.51.100.254/24 dev gw-veth1
-ip netns exec ns2 ip address add 198.51.100.1/24 dev ns2-veth0
-ip netns exec ns1 ip addr show ns1-veth0 | tee output.log
-ip netns exec router ip addr show gw-veth0 | tee -a output.log
-ip netns exec router ip addr show gw-veth1 | tee -a output.log
-ip netns exec ns2 ip addr show ns2-veth0 | tee -a output.log
+# ip netns exec ns1 ip address add 192.0.2.1/24 dev ns1-veth0
+# ip netns exec router ip address add 192.0.2.254/24 dev gw-veth0
+# ip netns exec router ip address add 198.51.100.254/24 dev gw-veth1
+# ip netns exec ns2 ip address add 198.51.100.1/24 dev ns2-veth0
+# ip netns exec ns1 ip addr show ns1-veth0 | tee output.log
+# ip netns exec router ip addr show gw-veth0 | tee -a output.log
+# ip netns exec router ip addr show gw-veth1 | tee -a output.log
+# ip netns exec ns2 ip addr show ns2-veth0 | tee -a output.log
+
+ip netns exec ns1 ip route add default via 192.0.2.254
+ip netns exec ns2 ip route add default via 198.51.100.254
+ip netns exec ns1 ip route show | tee output.log
+ip netns exec ns2 ip route show | tee -a output.log
