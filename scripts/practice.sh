27544184562a7d3a29986e0990fd91576ccd3025
maton369 <ysk901085@gmail.com>
2025-06-04 13:09:00 +0900

    Add: インターフェースをUP

--- scripts/practice.sh
+++ scripts/practice.sh
@@ -9,10 +9,16 @@
 # ip link add ns2-veth0 type veth peer name gw-veth1
 # ip link show | grep veth | tee output.log
 
-ip link set ns1-veth0 netns ns1
-ip link set gw-veth0 netns router
-ip link set gw-veth1 netns router
-ip link set ns2-veth0 netns ns2
-ip netns exec ns1 ip link show | tee output.log
-ip netns exec router ip link show | tee -a output.log
-ip netns exec ns2 ip link show | tee -a output.log
+# ip link set ns1-veth0 netns ns1
+# ip link set gw-veth0 netns router
+# ip link set gw-veth1 netns router
+# ip link set ns2-veth0 netns ns2
+# ip netns exec ns1 ip link show | tee output.log
+# ip netns exec router ip link show | tee -a output.log
+# ip netns exec ns2 ip link show | tee -a output.log
+
+ip netns exec ns1 ip link set ns1-veth0 up
+ip netns exec router ip link set gw-veth0 up
+ip netns exec router ip link set gw-veth1 up
+ip netns exec ns2 ip link set ns2-veth0 up
+ip netns exec ns1 ip link show ns1-veth0 | grep state | tee output.log
