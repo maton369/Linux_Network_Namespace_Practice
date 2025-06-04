3460641ad8a1e909e700eee2e64fa1011dc29d2a
maton369 <ysk901085@gmail.com>
2025-06-04 13:06:33 +0900

    Add: インターフェースをNetwork Namespaceに所属

--- scripts/practice.sh
+++ scripts/practice.sh
@@ -5,6 +5,14 @@
 # ip netns add ns2
 # ip netns list | tee output.log
 
-ip link add ns1-veth0 type veth peer name gw-veth0
-ip link add ns2-veth0 type veth peer name gw-veth1
-ip link show | grep veth | tee output.log
+# ip link add ns1-veth0 type veth peer name gw-veth0
+# ip link add ns2-veth0 type veth peer name gw-veth1
+# ip link show | grep veth | tee output.log
+
+ip link set ns1-veth0 netns ns1
+ip link set gw-veth0 netns router
+ip link set gw-veth1 netns router
+ip link set ns2-veth0 netns ns2
+ip netns exec ns1 ip link show | tee output.log
+ip netns exec router ip link show | tee -a output.log
+ip netns exec ns2 ip link show | tee -a output.log
