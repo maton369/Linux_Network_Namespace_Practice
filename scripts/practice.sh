4e1a9758a24537e0b7522b07933617c6c7579078
maton369 <ysk901085@gmail.com>
2025-06-04 13:51:11 +0900

    Add: インターフェースの作成

--- scripts/practice.sh
+++ scripts/practice.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-ip netns add ns1
-ip netns add ns2
-ip netns add ns3
-ip netns add bridge
-ip netns list | tee output.log
+# ip netns add ns1
+# ip netns add ns2
+# ip netns add ns3
+# ip netns add bridge
+# ip netns list | tee output.log
+
+ip link add ns1-veth0 type veth peer name ns1-br0
+ip link add ns2-veth0 type veth peer name ns2-br0
+ip link add ns3-veth0 type veth peer name ns3-br0
+ip link show | grep veth | tee output.log
