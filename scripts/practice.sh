9bd78eb416f528a85175b0ac65803213e1b2d6bf
maton369 <ysk901085@gmail.com>
2025-06-05 10:26:39 +0900

    Add: ループバックアドレスのドメイン名を確認

--- scripts/practice.sh
+++ scripts/practice.sh
@@ -2,4 +2,6 @@
 
 # python3 -m http.server 8000 --bind 127.0.0.1 --directory ../tmp/http-home
 # echo -en "GET / HTTP/1.0\r\n\r\n" | nc 127.0.0.1 8000 | tee output.log
-curl -X GET -D - http://127.0.0.1:8000 | tee -a output.log
+# curl -X GET -D - http://127.0.0.1:8000 | tee -a output.log
+
+ping -c 3 localhost | tee output.log
