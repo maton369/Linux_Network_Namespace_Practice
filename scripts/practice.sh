fcf08dbe6d20f5b2c3e7209bf104343967e56652
maton369 <ysk901085@gmail.com>
2025-06-05 10:22:32 +0900

    Add: HTTP通信できることを確認

--- scripts/practice.sh
+++ scripts/practice.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-# nc -ulnv 127.0.0.1 54321
-
-nc -lnv 127.0.0.1 54321
+# python3 -m http.server 8000 --bind 127.0.0.1 --directory ../tmp/http-home
+echo -en "GET / HTTP/1.0\r\n\r\n" | nc 127.0.0.1 8000 | tee output.log
