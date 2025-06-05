1768492cc997f8f4f16a677435370cf4ec8097af
maton369 <ysk901085@gmail.com>
2025-06-05 09:16:19 +0900

    Add: インターフェースをNetwork Namespaceに所属

--- scripts/practice.sh
+++ scripts/practice.sh
@@ -6,7 +6,15 @@
 # ip netns add bridge
 # ip netns list | tee output.log
 
-ip link add ns1-veth0 type veth peer name ns1-br0
-ip link add ns2-veth0 type veth peer name ns2-br0
-ip link add ns3-veth0 type veth peer name ns3-br0
+# ip link add ns1-veth0 type veth peer name ns1-br0
+# ip link add ns2-veth0 type veth peer name ns2-br0
+# ip link add ns3-veth0 type veth peer name ns3-br0
+# ip link show | grep veth | tee output.log
+
+ip link set ns1-veth0 netns ns1
+ip link set ns2-veth0 netns ns2
+ip link set ns3-veth0 netns ns3
+ip link set ns1-br0 netns bridge
+ip link set ns2-br0 netns bridge
+ip link set ns3-br0 netns bridge
 ip link show | grep veth | tee output.log
