a879d1026fc67af9e1bb5f380d33ed4a5247ffc5
maton369 <ysk901085@gmail.com>
2025-06-05 09:17:11 +0900

    Add: インターフェースをUP

--- scripts/practice.sh
+++ scripts/practice.sh
@@ -11,10 +11,18 @@
 # ip link add ns3-veth0 type veth peer name ns3-br0
 # ip link show | grep veth | tee output.log
 
-ip link set ns1-veth0 netns ns1
-ip link set ns2-veth0 netns ns2
-ip link set ns3-veth0 netns ns3
-ip link set ns1-br0 netns bridge
-ip link set ns2-br0 netns bridge
-ip link set ns3-br0 netns bridge
+# ip link set ns1-veth0 netns ns1
+# ip link set ns2-veth0 netns ns2
+# ip link set ns3-veth0 netns ns3
+# ip link set ns1-br0 netns bridge
+# ip link set ns2-br0 netns bridge
+# ip link set ns3-br0 netns bridge
+# ip link show | grep veth | tee output.log
+
+ip netns exec ns1 ip link set ns1-veth0 up
+ip netns exec ns2 ip link set ns2-veth0 up
+ip netns exec ns3 ip link set ns3-veth0 up
+ip netns exec bridge ip link set ns1-br0 up
+ip netns exec bridge ip link set ns2-br0 up
+ip netns exec bridge ip link set ns3-br0 up
 ip link show | grep veth | tee output.log
