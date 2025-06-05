0d2da900aa40b78703ddf3129085452528da7af3
maton369 <ysk901085@gmail.com>
2025-06-05 09:18:51 +0900

    Add: インターフェースにIPアドレスを付与

--- scripts/practice.sh
+++ scripts/practice.sh
@@ -19,10 +19,17 @@
 # ip link set ns3-br0 netns bridge
 # ip link show | grep veth | tee output.log
 
-ip netns exec ns1 ip link set ns1-veth0 up
-ip netns exec ns2 ip link set ns2-veth0 up
-ip netns exec ns3 ip link set ns3-veth0 up
-ip netns exec bridge ip link set ns1-br0 up
-ip netns exec bridge ip link set ns2-br0 up
-ip netns exec bridge ip link set ns3-br0 up
-ip link show | grep veth | tee output.log
+# ip netns exec ns1 ip link set ns1-veth0 up
+# ip netns exec ns2 ip link set ns2-veth0 up
+# ip netns exec ns3 ip link set ns3-veth0 up
+# ip netns exec bridge ip link set ns1-br0 up
+# ip netns exec bridge ip link set ns2-br0 up
+# ip netns exec bridge ip link set ns3-br0 up
+# ip link show | grep veth | tee output.log
+
+ip netns exec ns1 ip address add 192.0.2.1/24 dev ns1-veth0
+ip netns exec ns2 ip address add 192.0.2.2/24 dev ns2-veth0
+ip netns exec ns3 ip address add 192.0.2.3/24 dev ns3-veth0
+ip netns exec ns1 ip link show ns1-veth0 | grep state | tee output.log
+ip netns exec ns2 ip link show ns2-veth0 | grep state | tee -a output.log
+ip netns exec ns3 ip link show ns3-veth0 | grep state | tee -a output.log
