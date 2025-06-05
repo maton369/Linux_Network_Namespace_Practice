a902f48410a5717fa580257bd61776da50720fae
maton369 <ysk901085@gmail.com>
2025-06-05 10:24:13 +0900

    Add: curlコマンドでも同様なのを確認

--- scripts/practice.sh
+++ scripts/practice.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 # python3 -m http.server 8000 --bind 127.0.0.1 --directory ../tmp/http-home
-echo -en "GET / HTTP/1.0\r\n\r\n" | nc 127.0.0.1 8000 | tee output.log
+# echo -en "GET / HTTP/1.0\r\n\r\n" | nc 127.0.0.1 8000 | tee output.log
+curl -X GET -D - http://127.0.0.1:8000 | tee -a output.log
