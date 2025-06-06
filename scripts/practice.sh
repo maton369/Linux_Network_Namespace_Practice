e8b3e72e606c8d669ef45f892167f75251d85686
maton369 <ysk901085@gmail.com>
2025-06-06 17:07:38 +0900

    Add: インターフェースをNetwork Namespaceに追加

--- scripts/practice.sh
+++ scripts/practice.sh
@@ -5,6 +5,12 @@
 # ip netns add wan
 # ip netns list | tee output.log
 
-ip link add lan-veth0 type veth peer name gw-veth0
-ip link add wan-veth0 type veth peer name gw-veth1
-ip link show | grep veth | tee output.log
+# ip link add lan-veth0 type veth peer name gw-veth0
+# ip link add wan-veth0 type veth peer name gw-veth1
+# ip link show | grep veth | tee output.log
+
+ip link set lan-veth0 netns lan
+ip link set gw-veth0 netns router
+ip link set gw-veth1 netns router
+ip link set wan-veth0 netns wan
+ip netns exec lan ip link show | tee output.log
