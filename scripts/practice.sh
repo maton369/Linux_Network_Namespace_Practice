470af12685557f8ec75ab96af9c3c320e7a20c9b
maton369 <ysk901085@gmail.com>
2025-06-06 17:06:21 +0900

    Add: インターフェスを作成

--- scripts/practice.sh
+++ scripts/practice.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-ip netns add lan
-ip netns add router
-ip netns add wan
-ip netns list | tee output.log
+# ip netns add lan
+# ip netns add router
+# ip netns add wan
+# ip netns list | tee output.log
+
+ip link add lan-veth0 type veth peer name gw-veth0
+ip link add wan-veth0 type veth peer name gw-veth1
+ip link show | grep veth | tee output.log
