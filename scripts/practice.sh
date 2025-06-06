633132bb7099aa1a2e0402ceb5b104921f228f64
maton369 <ysk901085@gmail.com>
2025-06-06 17:08:29 +0900

    Add: インターフェースをUP

--- scripts/practice.sh
+++ scripts/practice.sh
@@ -9,8 +9,14 @@
 # ip link add wan-veth0 type veth peer name gw-veth1
 # ip link show | grep veth | tee output.log
 
-ip link set lan-veth0 netns lan
-ip link set gw-veth0 netns router
-ip link set gw-veth1 netns router
-ip link set wan-veth0 netns wan
-ip netns exec lan ip link show | tee output.log
+# ip link set lan-veth0 netns lan
+# ip link set gw-veth0 netns router
+# ip link set gw-veth1 netns router
+# ip link set wan-veth0 netns wan
+# ip netns exec lan ip link show | tee output.log
+
+ip netns exec lan ip link set lan-veth0 up
+ip netns exec router ip link set gw-veth0 up
+ip netns exec router ip link set gw-veth1 up
+ip netns exec wan ip link set wan-veth0 up
+ip netns exec lan ip link show lan-veth0 | grep state | tee output.log
