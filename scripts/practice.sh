3858aa1533f2de3537d929646779ab40d3fef4f4
maton369 <ysk901085@gmail.com>
2025-06-07 20:32:21 +0900

    Add: ホストバイトオーダーとネットワークバイトオーダーの変換

--- scripts/practice.sh
+++ scripts/practice.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
 # python3 echoserver.py
-nc 127.0.0.1 54321 | tee -a output.log
+# nc 127.0.0.1 54321 | tee -a output.log
+
+# python3 addserver.py
+python3 addclient.py | tee output.log
